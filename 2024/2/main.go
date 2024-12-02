50d9030e7ab551a21d8f7163956c5b6722c6d310
LuisLSousa <luis.srl.sousa@gmail.com>
2024-12-03 00:12:23 +0100

    day 2

--- 2024/2/main.go
+++ 2024/2/main.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const inputFileName = "input.txt"
+
+func isSafe(row []string) bool {
+	inc := make([]int, len(row)-1)
+	for i := 0; i < len(row)-1; i++ {
+		curr, _ := strconv.Atoi(row[i])
+		next, _ := strconv.Atoi(row[i+1])
+		inc[i] = next - curr
+	}
+
+	increasingSet := true
+	decreasingSet := true
+	for _, v := range inc {
+		if v < 1 || v > 3 {
+			increasingSet = false
+		}
+		if v > -1 || v < -3 {
+			decreasingSet = false
+		}
+	}
+
+	return increasingSet || decreasingSet
+}
 
 func main() {
 	file, err := os.Open(inputFileName)
@@ -18,12 +43,23 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	scanner.Split(bufio.ScanLines)
+	var part1Sum, part2Sum int
 	for scanner.Scan() {
-		// do stuff
+		report := strings.Fields(scanner.Text())
+		if isSafe(report) {
+			part1Sum++
+		}
+
+		for i := range report {
+			newReport := append([]string{}, report[:i]...)
+			newReport = append(newReport, report[i+1:]...)
+			if isSafe(newReport) {
+				part2Sum++
+				break
+			}
+		}
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatalf("error scanning file: %s", err)
-	}
+	fmt.Println(part1Sum)
+	fmt.Println(part2Sum)
 }
